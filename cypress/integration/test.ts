2576a2df1582a7714687c536759d7ffaca9a13c8
David Salazar <dssalaza@gmail.com>
2021-08-16 12:59:17 -0500

    feat: added assertion for login test

--- cypress/integration/test.ts
+++ cypress/integration/test.ts
@@ -1,9 +1,12 @@
 import { LoginPage } from "../page-objects/login";
+import { HomePage } from "../page-objects/home-page";
+
 import '../support/commands/login'
 
 describe('Login using TypeScript', () => {
 
     const loginPage = new LoginPage()
+    const homePage = new HomePage()
     let users;
 
     beforeEach(() => {
@@ -15,6 +18,8 @@ describe('Login using TypeScript', () => {
     it('Login as standar user', () => {
         loginPage.navigate();
         cy.login(users.standardUser , Cypress.env('user_password'));
+        
+        homePage.getBurgerMenu().should('be.visible');
     })
 })
  
